57283cded1b148dbcb73028edcd7a43b608531e8
guinhoanjos <iago.anjos@hotmail.com>
2021-12-17 18:04:40 -0300

    implementado test unitário da funcao de ordenar numeros

---
+++ tests/ordernar.test.js
@@ -0,0 +1,28 @@
+const ordenarFunction = require('../../funcao organizar em ordem numerica/sort')
+
+
+
+describe('Ordenando numeros', () =>{
+  
+  test('com numeros inteiros', () => {
+    const array = [ 3, 2 , 1 ];
+    const arrayOrdernado = [ 1, 2, 3];
+    
+    expect(ordenarFunction(array)).toEqual(arrayOrdernado);
+  });
+
+  test('com numeros negativos', () => {
+    const array = [-5, -10, -15, -2, -13 ];
+    const arrayOrdernado = [ -15, -13, -10, -5, -2];
+    
+    expect(ordenarFunction(array)).toEqual(arrayOrdernado);
+  });
+
+  test('com numeros negativos, reais e inteiros', () => {
+    const array = [ 0.1, 2, -3, 5, 4.5 , -0.1, 1];
+    const arrayOrdernado = [ -3, -0.1, 0.1, 1, 2, 4.5, 5];
+
+    expect(ordenarFunction(array)).toEqual(arrayOrdernado);
+  });
+
+})
